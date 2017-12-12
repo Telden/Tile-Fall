1c6cf33d943fd072512fc8684f72f91941801add
Nicholas Robbins <nicholas.robbins@mymail.champlain.edu>
2017-12-12 00:27:38 -0500

    Music and controls overlay

--- Tile-Fall/Assets/Scripts/UI_Script.cs
+++ Tile-Fall/Assets/Scripts/UI_Script.cs
@@ -10,6 +10,7 @@ public class UI_Script : MonoBehaviour {
     Image left, right;
     public Sprite left00, left01, left02, left03, right00, right01, right02, right03;
     int leftMove, rightMove;
+	public SpriteRenderer Controls;
 
 
 	// Use this for initialization
@@ -19,58 +20,76 @@ public class UI_Script : MonoBehaviour {
 		blueWin.SetActive (false);
 		redWin.SetActive (false);
 		draw.SetActive (false);
+		Controls.color = new Color(Controls.color.r, Controls.color.g, Controls.color.b, 0);
     }
 	
 	// Update is called once per frame
 	void Update () {
-        leftMove = turnControler.GetComponent<TurnController>().player1Movement;
-        rightMove = turnControler.GetComponent<TurnController>().player2Movement;
-        switch (leftMove) {
-            case 3:
-                {
-                    left.sprite = left00;
-                    break;
-                }
-            case 2:
-                {
-                    left.sprite = left01;
-                    break;
-                }
-            case 1:
-                {
-                    left.sprite = left02;
-                    break;
-                }
-            case 0:
-                {
-                    left.sprite = left03;
-                    break;
-                }
-        }
-        switch (rightMove)
-        {
-            case 3:
-                {
-                    right.sprite = right00;
-                    break;
-                }
-            case 2:
-                {
-                    right.sprite = right01;
-                    break;
-                }
-            case 1:
-                {
-                    right.sprite = right02;
-                    break;
-                }
-            case 0:
-                {
-                    right.sprite = right03;
-                    break;
-                }
-        }
+		updateUI();
+		checkInput();
     }
+
+	void checkInput()
+	{
+		if(Input.GetButton("Controls"))
+		{
+			Controls.color = new Color(Controls.color.r, Controls.color.g, Controls.color.b, 1);
+		}
+		else
+		{
+			Controls.color = new Color(Controls.color.r, Controls.color.g, Controls.color.b, 0);
+		}
+	}
+	void updateUI()
+	{
+		leftMove = turnControler.GetComponent<TurnController>().player1Movement;
+		rightMove = turnControler.GetComponent<TurnController>().player2Movement;
+		switch (leftMove) {
+		case 3:
+			{
+				left.sprite = left00;
+				break;
+			}
+		case 2:
+			{
+				left.sprite = left01;
+				break;
+			}
+		case 1:
+			{
+				left.sprite = left02;
+				break;
+			}
+		case 0:
+			{
+				left.sprite = left03;
+				break;
+			}
+		}
+		switch (rightMove)
+		{
+		case 3:
+			{
+				right.sprite = right00;
+				break;
+			}
+		case 2:
+			{
+				right.sprite = right01;
+				break;
+			}
+		case 1:
+			{
+				right.sprite = right02;
+				break;
+			}
+		case 0:
+			{
+				right.sprite = right03;
+				break;
+			}
+		}
+	}
 
 	public void Restart() {
 		SceneManager.LoadScene ("Scene_1");
